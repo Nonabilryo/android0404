3b192914e7e3e24c0a6e19d52271ce0aa5bfc683
bushrooom <bushrooom@dgsw.hs.kr>
2024-05-02 20:15:41 +0900

    아이디 찾기 뷰

---
+++ app/src/main/java/kr/hs/dgsw/nonabilryo/IdFindActivity.kt
@@ -0,0 +1,16 @@
+package kr.hs.dgsw.nonabilryo
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class IdFindActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_id_find_acivity)
+
+    }
+}
