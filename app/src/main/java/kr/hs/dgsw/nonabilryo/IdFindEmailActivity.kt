4b894b272c488e9dca85d0176ea4d8ed459a9364
Jeong-Yeowon <yw112912@dgsw.hs.kr>
2024-05-16 19:39:24 +0900

    ⚡️ :: Login&Signup

---
+++ app/src/main/java/kr/hs/dgsw/nonabilryo/IdFindEmailActivity.kt
@@ -0,0 +1,20 @@
+package kr.hs.dgsw.nonabilryo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class IdFindEmailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_id_find_email)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
