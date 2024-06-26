9a113864fd68725324b6c12b637bfa349935cee1
bushrooom <bushrooom@dgsw.hs.kr>
2024-06-26 16:37:51 +0900

    마이페이지수정

--- app/src/main/java/kr/hs/dgsw/nonabilryo/my/GridAdapter.kt
+++ app/src/main/java/kr/hs/dgsw/nonabilryo/my/GridAdapter.kt
@@ -6,8 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.TextView
 import kr.hs.dgsw.nonabilryo.R
 
 class GridAdapter(private val context: Context) : BaseAdapter() {
