71c3da84e184560f1a54fb5758b018235887cbab
yeo-won <yw112912@dgsw.hs.kr>
2024-07-23 11:08:42 +0900

    ⚡️ :: RetrofitClient
    수정

--- app/src/main/java/kr/hs/dgsw/nonabilryo/RetrofitClient.kt
+++ app/src/main/java/kr/hs/dgsw/nonabilryo/RetrofitClient.kt
@@ -45,7 +45,7 @@ interface RetrofitService {
 }
 
 object RetrofitClient {
-    private const val BASE_URL = "https://10.80.161.181:8080/"
+    private const val BASE_URL = "http://10.80.161.181:8080/"
 
     private val okHttpClient: OkHttpClient by lazy {
         val trustAllCertificates = arrayOf<TrustManager>(object : X509TrustManager {
